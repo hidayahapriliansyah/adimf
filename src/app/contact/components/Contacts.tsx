7b70cdf482f37f2821dc36cff15cec74e5441335
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-08-13 08:37:33 +0700

    fix: contact & socials styling

--- src/app/contact/components/Contacts.tsx
+++ src/app/contact/components/Contacts.tsx
@@ -2,6 +2,10 @@
 
 import React from 'react'
 import { motion } from 'framer-motion'
+import { BiLogoGmail } from "react-icons/bi";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 const Contacts = () => {
   return (
@@ -11,21 +15,21 @@ const Contacts = () => {
       transition={{ duration: 0.9, delay: 1.8 }}
       className='mt-8 h-96'
     >
-      <ul className='text-xl md:text-2xl'>
-        <li>
-          Email:{' '}
+      <ul className='flex flex-col gap-4 text-lg md:text-2xl'>
+        <li className='flex flex-wrap justify-start items-center gap-4'>
+          <BiLogoGmail className='w-6 h-6 md:w-8 md:h-8 inline' />
           <a href="mailto:adimuhamadfirmansyah@gmail.com" className='hover:underline'>adimuhamadfirmansyah@gmail.com</a>
         </li>
-        <li>
-          LinkedIn:{' '}
+        <li className='flex flex-wrap justify-start items-center gap-4'>
+          <FaLinkedinIn className='w-6 h-6 md:w-8 md:h-8 inline' />
           <a href="https://www.linkedin.com/in/hidayahapriliansyah/" target='_blank' className='hover:underline'>Adi Muhamad Firmansyah</a>
         </li>
-        <li>
-          Github:{' '}
+        <li className='flex flex-wrap justify-start items-center gap-4'>
+          <FaGithub className='w-6 h-6 md:w-8 md:h-8 inline' />
           <a href="https://github.com/hidayahapriliansyah" target='_blank' className='hover:underline'>hidayahapriliansyah</a>
         </li>
-        <li>
-          Instagram:{' '}
+        <li className='flex flex-wrap justify-start items-center gap-4'>
+          <FaInstagram className='w-6 h-6 md:w-8 md:h-8 inline' />
           <a href="https://www.instagram.com/hidayahapriliansyah/" target='_blank' className='hover:underline'>hidayahapriliansyah</a>
         </li>
       </ul>
