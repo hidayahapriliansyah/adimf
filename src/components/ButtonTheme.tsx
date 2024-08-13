7f83d33a873d9756ef9e4693146a0905a36a1231
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-08-13 14:37:20 +0700

    chore: change button theme strategy

--- src/components/ButtonTheme.tsx
+++ src/components/ButtonTheme.tsx
@@ -1,37 +1,22 @@
 "use client"
 
-import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
-import { Ellipsis } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
 export function ButtonTheme() {
-  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+  const { setTheme, resolvedTheme } = useTheme()
 
-  const [mounted, setMounted] = React.useState(false)
+  useEffect(() => setMounted(true), [])
 
-  // Ensure the component is only mounted on the client
-  React.useEffect(() => {
-    setMounted(true)
-  }, [])
+  if (!mounted) return <div className='w-5 h-5' />
 
-  if (!mounted) return <button className="flex items-center justify-center p-2 rounded-md bg-zinc-200 dark:bg-zinc-800 transition-colors"
-  >
-    <Ellipsis className="h-5 w-5 text-zinc-800 dark:text-zinc-200" />
-    <span className="sr-only">Loading theme</span>
-  </button>
+  if (resolvedTheme === 'dark') {
+    return <Sun className='w-5 h-5' onClick={() => setTheme('light')} />
+  }
 
-  return (
-    <button
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="flex items-center justify-center p-2 rounded-md bg-zinc-200 dark:bg-zinc-800 transition-colors"
-    >
-      {theme === "dark" ? (
-        <Sun className="h-5 w-5 text-zinc-800 dark:text-zinc-200" />
-      ) : (
-        <Moon className="h-5 w-5 text-zinc-800 dark:text-zinc-200" />
-      )}
-      <span className="sr-only">Toggle theme</span>
-    </button>
-  )
+  if (resolvedTheme === 'light') {
+    return <Moon className='w-5 h-5' onClick={() => setTheme('dark')} />
+  }
 }
