7e1d843baa9fe34be945bb7148c606a0b36f58a5
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-08-20 20:01:29 +0700

    feat: add gib tasik on projects

--- src/app/projects/components/ProjectsList.tsx
+++ src/app/projects/components/ProjectsList.tsx
@@ -19,11 +19,41 @@ const ProjectsList = () => {
 
       <div className='flex flex-col justify-between items-start gap-2 md:gap-4 py-4 border border-input rounded-lg'>
         <div className='flex flex-col gap-4 w-full'>
+          <span className='text-lg md:text-2xl font-semibold pt-0 px-4'>GIB Tasikmalaya</span>
+          <div className='relative w-full aspect-video'>
+            <Image
+              src={'/projects/gib.png'}
+              alt='GIB Tasikmalaya'
+              fill
+              className='object-cover'
+            />
+          </div>
+          <p className='md:text-xl px-4'>Gerakan Infaq Tasikmalaya website that contains detail, contact, and more.</p>
+          <div className='flex gap-2 px-4 mb-4'>
+            <StackIcon name="typescript" className='w-6 sm:w-8' />
+            <StackIcon name="nextjs2" className='w-6 sm:w-8' />
+          </div>
+        </div>
+        <div className='flex justify-between w-full px-4'>
+          <a href='https://gib-tasikmalaya.vercel.app/' target='_blank'>
+            <Button className='md:text-lg'>
+              Live
+              <ExternalLink className='ml-2' />
+            </Button>
+          </a>
+          <a href='https://github.com/hidayahapriliansyah/gib-tasikmalaya' target='_blank' className='flex justify-center items-center text-zinc-950 dark:text-white'>
+            <Github className='ml-2' />
+          </a>
+        </div>
+      </div>
+
+      <div className='flex flex-col justify-between items-start gap-2 md:gap-4 py-4 border border-input rounded-lg'>
+        <div className='flex flex-col gap-4 w-full'>
           <span className='text-lg md:text-2xl font-semibold pt-0 px-4'>Open Music API</span>
           <div className='relative w-full aspect-video'>
             <Image
               src={'/projects/node.jpg'}
-              alt='Amatrace'
+              alt='Open Music API'
               fill
               className='object-cover'
             />
