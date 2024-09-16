649d40fa31a47c026785238477f23ab022ff9553
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-09-16 09:18:37 +0700

    chore: refactoring

---
+++ src/app/data/projects.ts
@@ -0,0 +1,99 @@
+export type Project = {
+  title: string;
+  image: string;
+  description: string;
+  stack: string[];
+  liveUrl: string | null;
+  github: string | null;
+}
+
+export const projects: Project[] = [
+  {
+    title: "Dicoding Clone",
+    image: "dicoding-clone.jpeg",
+    description: "Dicoding home page clone.",
+    stack: ["typescript", "nextjs2", "tailwindcss", "framer"],
+    liveUrl: "https://dicoding-clone.vercel.app/",
+    github: "https://github.com/hidayahapriliansyah/dicoding-clone"
+  },
+  {
+    title: "Thumbhash Generator",
+    image: "thumbhash.png",
+    description: "Effortless to create your customize beautiful and small blur image placeholder.",
+    stack: ["typescript", "nextjs2", "tailwindcss"],
+    liveUrl: "https://thumbhash-generator.vercel.app/",
+    github: "https://github.com/hidayahapriliansyah/thumbhash-generator"
+  },
+  {
+    title: "GIB Tasikmalaya",
+    image: "gib.png",
+    description: "Gerakan Infaq Tasikmalaya website that contains detail, contact, and more.",
+    stack: ["typescript", "nextjs2"],
+    liveUrl: "https://gib-tasikmalaya.vercel.app/",
+    github: "https://github.com/hidayahapriliansyah/gib-tasikmalaya"
+  },
+  {
+    title: "Open Music API",
+    image: "node.jpg",
+    description: "Open Music API with PostgreSQL, Redis, RabbitMQ and E2E testing with Jest.",
+    stack: ["nodejs", "redis", "postgresql", "jest"],
+    liveUrl: null,
+    github: "https://github.com/hidayahapriliansyah/oper-music-api"
+  },
+  {
+    title: "AmaTrace",
+    image: "amatrace.png",
+    description: "My Bangkit Capstone project team is collaborating with Amati Indonesia to build a Traceability Platform for SMEs.",
+    stack: ["typescript", "nextjs2", "postgresql", "gcloud"],
+    liveUrl: null,
+    github: "https://github.com/AMATI-TP-02"
+  },
+  {
+    title: "Link Tree Clone",
+    image: "linktree-clone.png",
+    description: "Simple Link Tree clone with Glassmorphism design using all vanilla code (HTML, CSS, JS).",
+    stack: ["html5", "css3", "js"],
+    liveUrl: "https://linktree-clone-it-insight-day.vercel.app/",
+    github: "https://github.com/hidayahapriliansyah/linktree-clone"
+  },
+  {
+    title: "Ngifent",
+    image: "contoh.png",
+    description: "E-Ticket platform for managing community event.",
+    stack: ["nodejs", "typescript", "nextjs2", "postgresql", "prisma"],
+    liveUrl: "https://ngifent.com",
+    github: null
+  },
+  {
+    title: "Realtime Chat with Pusher",
+    image: "realtime.png",
+    description: "Fulstack realtime chatting app with Pusher and OAuth.",
+    stack: ["nextjs2"],
+    liveUrl: "https://realtime-messanger-nextjs.vercel.app/",
+    github: "https://github.com/hidayahapriliansyah/realtime-messanger-nextjs"
+  },
+  {
+    title: "Hoobank Landing Page",
+    image: "hoobank.png",
+    description: "Slicing Beautiful Hoobank Landing Page with React.js.",
+    stack: ["vitejs", "reactjs"],
+    liveUrl: "https://bank-modern-app-pi-jet.vercel.app/",
+    github: "https://github.com/hidayahapriliansyah/bank-modern-app-prod"
+  },
+  {
+    title: "Open AI Codex",
+    image: "codex.png",
+    description: "Chatting with Codex powered by Open API.",
+    stack: ["openai", "reactjs"],
+    liveUrl: "https://hidayahapr-open-ai.vercel.app/",
+    github: "https://github.com/hidayahapriliansyah/open_ai_codex/tree/master"
+  },
+  {
+    title: "Prakiraan Cuaca",
+    image: "prakiraan.jpeg",
+    description: "Weather forecasting using openweathermap.org",
+    stack: ["html5", "css3", "js"],
+    liveUrl: null,
+    github: null
+  }
+];
---
+++ src/app/projects/components/ProjectCard.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import type { Project } from '../../data/projects'
+import Image from 'next/image'
+import StackIcon from 'tech-stack-icons'
+import { Button } from '../../../components/ui/button'
+import { ExternalLink, Github } from 'lucide-react'
+
+const ProjectCard = (project: Project) => {
+  return (
+    <div className='flex flex-col justify-between items-start gap-2 md:gap-4 py-4 border border-input rounded-lg'>
+      <div className='flex flex-col gap-4 w-full'>
+        <span className='text-lg md:text-2xl font-semibold pt-0 px-4'>{project.title}</span>
+        <div className='relative w-full aspect-video'>
+          <Image
+            src={`/projects/${project.image}`}
+            alt={project.title}
+            fill
+            className='object-cover'
+          />
+        </div>
+        <p className='md:text-xl px-4'>{project.description}</p>
+        <div className='flex gap-2 px-4 mb-4'>
+          {
+            project.stack.map((stack, i) => (
+              <StackIcon key={i} name={stack} className='w-6 sm:w-8' />
+            ))
+          }
+        </div>
+      </div>
+      <div className='flex justify-between w-full px-4'>
+        <div className='w-fit h-fit'>
+          {
+            project.liveUrl &&
+            <a href={project.liveUrl} target='_blank'>
+              <Button className='md:text-lg'>
+                Live
+                <ExternalLink className='ml-2' />
+              </Button>
+            </a>
+          }
+        </div>
+        <div className='w-fit h-fit'>
+          {
+            project.github &&
+            <a href={project.github} target='_blank' className='flex justify-center items-center text-zinc-950 dark:text-white'>
+              <Github className='ml-2' />
+            </a>
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectCard
